5dbdc8bf2de34b779c77ff07fe2ce0c167da0fe0
DenysKors <105918404+DenysKors@users.noreply.github.com>
2022-10-07 16:11:39 +0300

    Create Gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,35 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divGalleryRef = document.querySelector('.gallery')
+
+
+
+// const instance = basicLightbox.create(`
+//    <img src="assets/images/image.png">
+// `)
+
+// divGalleryRef.addEventListener('click', instance)
+// instance.show()
+
+
+function createGallerySet() {
+    const galleryLayout = galleryItems
+        .map(({ preview, original, description }) =>  
+            `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+            </div>`)
+        .join('')
+
+    divGalleryRef.innerHTML = galleryLayout;
+}
+
+createGallerySet()
